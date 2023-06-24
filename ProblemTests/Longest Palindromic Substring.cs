4ccf3ceb94a71cc1af05d496a29281ba0965592c
Ruslan Rybka <ruslan.rybka@capgemini.com>
2023-06-25 00:41:18 +0300

    4

--- ProblemTests/Longest Palindromic Substring.cs
+++ ProblemTests/Longest Palindromic Substring.cs
@@ -4,6 +4,12 @@ public class Longest_Palindromic_Substring
 {
     [Theory]
     [InlineData("babad", "bab")]
+    [InlineData("cbbd", "bb")]
+    [InlineData("ac", "a")]
+    [InlineData("bb", "bb")]
+    [InlineData("ccc", "ccc")]
+    [InlineData("aaaa", "aaaa")]
+    [InlineData("aacabdkacaa", "aca")]
     public void Test(string input, string expected)
     {
         var s = new Solution();
@@ -16,9 +22,70 @@ public class Solution
 {
     public string LongestPalindrome(string s)
     {
-        for (int i = 0; i < s.Length; i++)
+        if (s.Length == 1)
+            return s;
+        var sbstrInd = 0;
+        var sbstrLen = 1;
+
+        for (int i = 0; i < s.Length - 1; i++)
         {
-            
+            var testOdd = true;
+            var testPair = true;
+
+            for (int chkOffst = 1; chkOffst <= s.Length / 2; chkOffst++)
+            {
+                var rt = i + chkOffst;
+
+                if (rt >= s.Length)
+                    break;
+
+                var lt = i - chkOffst;
+                var lt2 = i - chkOffst + 1;
+
+                var found = false;
+
+                if (lt2 >= 0 && s[lt2] == s[rt] && (chkOffst == 1 || testOdd))
+                {
+                    var len = rt - lt2 + 1;
+
+                    if (sbstrLen < len)
+                    {
+                        sbstrInd = lt2;
+                        sbstrLen = len;
+                    }
+
+                    found = true;
+                }
+                else
+                {
+                    testOdd = false;
+                }
+
+                if (lt >= 0 && s[lt] == s[rt] && (chkOffst == 1 || testPair))
+                {
+                    var len = rt - lt + 1;
+
+                    if (sbstrLen < len)
+                    {
+                        sbstrInd = lt;
+                        sbstrLen = len;
+                    }
+
+                    found = true;
+                }
+                else
+                {
+                    testPair = false;
+                }
+
+                if (!found)
+                    break;
+            }
         }
+
+        if (sbstrInd == -1)
+            return string.Empty;
+
+        return s.Substring(sbstrInd, sbstrLen);
     }
 }
---
+++ ProblemTests/Zigzag Conversion.cs
@@ -0,0 +1,79 @@
+﻿using Shouldly;
+
+public class Zigzag_Conversion
+{
+    [Theory]
+    [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+    [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+    public void Test(string input, int numRows, string expected)
+    {
+        var s = new Solution();
+        var result = s.Convert(input, numRows);
+        result.ShouldBe(expected);
+    }
+}
+
+public class Solution
+{
+    public string Convert(string s, int numRows)
+    {
+        var lastRowIdx = numRows - 1;
+        var result = new char[s.Length];
+        var writeInd = 0;
+
+        var iterX = 0;
+        var iterY = 0;
+        var iterDiag = false;
+        var iterIndex = 0;
+
+        var tgtX = -1;
+        var tgtY = 0;
+
+        while (writeInd < s.Length)
+        {
+            if (iterY == tgtY)
+            {
+                if (tgtX < iterX)
+                {
+                    tgtX = iterX;
+                    result[writeInd++] = s[iterIndex];
+                }
+            }
+         
+
+            if (!iterDiag)
+            {
+                iterY++;
+
+                if (iterY == lastRowIdx)
+                {
+                    iterDiag = true;
+                }
+            }
+            else
+            {
+                iterY--;
+                iterX++;
+
+                if (iterY == 0)
+                {
+                    iterDiag = false;
+                }
+            }
+
+            iterIndex++;
+
+            if (iterIndex == s.Length)
+            {
+                tgtY++;
+                tgtX = -1;
+                iterIndex = tgtY; //0?
+                iterX = 0;
+                iterY = tgtY;
+                iterDiag = iterY == lastRowIdx;
+            }
+        }
+
+        return new string(result);
+    }
+}
